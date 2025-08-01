0895f8cdd84f6fa1f2261e27edb9f0332a0f1f28
Levi Barroga <levibarroga18@gmail.com>
2025-08-01 20:25:15 +0800

    Remove test code comments.

--- job_listing/spiders/web_one.py
+++ job_listing/spiders/web_one.py
@@ -23,48 +23,14 @@ class WebOne(scrapy.Spider):
 
     async def parse(self, response):
         page = response.meta["playwright_page"]
-        # title = response.css('article::attr(aria-label)')
-        # title = await page.locator('[data-testid="job-card-title"]').all()
-        await page.wait_for_selector('article[aria-label]')  # Wait for JS content
+       
+        await page.wait_for_selector('article[aria-label]') 
         
-        # Get aria-labels from all jobcard articles
         articles = await page.locator('article[aria-label]').all()
         for article in articles:
             title = await article.get_attribute('aria-label')
             
             yield {"title": title.strip()}
         
-        # for titles in title_two:
-        #     titles = await title_two.getall()   
-        # for i, j in zip(articles, title_two):
-        #     title = await i.get_attribute("aria-label")
-        #     titles = await j.text_content("a")
-                 
-            
-        # hyper_link = page.locator('[data-testid="job-list-item-link-overlay"]').get("href").all()
-        # companies = page.locator('[data-type="company"]').get_by_role("link").all_text_contents()
-        # titles = response.css(await page.locator('[data-testid="job-card-title"]')).all_text_contents()
-        # titles = title.text_content()
-        # for i, j,k in zip(title, hyper_link, companies):
-        #     yield{
-        #         "job_title" : i.strip(),
-        #         "job_link" : j.get_attribute("href"),
-        #         "company" : k.strip()
-        #     }    
-            # my_list.append({
-            #     "title": job_title,
-            #     "hyperlink": job_link,
-            #     "company": company,
-            # })
-            # # yield {
-            #     "title": job_title,
-            #     "hyperlink": job_link,
-            #     "company": company,
-            # }
-        # return my_list
-        await page.close()
-
-        # yield{
-        #     "title": title.strip(),
-        # }
         
+        await page.close()
