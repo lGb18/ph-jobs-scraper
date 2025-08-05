fad303def97046b12eeade99ae107017dec0fb88
Levi Barroga <levibarroga18@gmail.com>
2025-08-05 15:40:32 +0800

    Add a Next Page loop.

--- job_listing/spiders/web_one.py
+++ job_listing/spiders/web_one.py
@@ -26,29 +26,32 @@ class WebOne(scrapy.Spider):
 
     async def parse(self, response):
         page = response.meta["playwright_page"]
-       
+        next_button = page.locator('a[rel="nofollow next"]')
+        
        #Jobstreet Job Card
-        articles = await page.locator('article').all()
-        
-        #Visible Job data Extraction
-        for article in articles:      
-            title = await article.locator('a[data-testid="job-card-title"]').text_content()
-            company = await article.locator('a[data-type="company"]').text_content()
-            job_link = await article.locator('a').first.get_attribute('href')
-            apply_link = await article.get_attribute('data-job-id')
-
-            locations = await article.locator('a[data-type="location"]').all()
-            city = await locations[0].text_content() if locations else "N/A"
-            region = await locations[1].text_content() if len(locations) > 1 else "N/A"
+        while next_button.is_visible:
+            articles = await page.locator('article').all()
             
-            base_url = "https://ph.jobstreet.com"
-            yield {"Job Title": title.strip(),
-                   "Company": company.strip(),
-                   "City": city.strip(),
-                   "Region": region.strip(),
-                   "Job Link": base_url+job_link.strip(),
-                   "Apply Link": base_url+"/job/"+apply_link+"/apply".strip()}
-        
-        
-              
+            #Visible Job data Extraction
+            for article in articles:      
+                title = await article.locator('a[data-testid="job-card-title"]').text_content()
+                company = await article.locator('a[data-type="company"]').text_content()
+                job_link = await article.locator('a').first.get_attribute('href')
+                apply_link = await article.get_attribute('data-job-id')
+
+                locations = await article.locator('a[data-type="location"]').all()
+                city = await locations[0].text_content() if locations else "N/A"
+                region = await locations[1].text_content() if len(locations) > 1 else "N/A"
+                
+                base_url = "https://ph.jobstreet.com"
+                yield {"Job Title": title.strip(),
+                    "Company": company.strip(),
+                    "City": city.strip(),
+                    "Region": region.strip(),
+                    "Job Link": base_url+job_link.strip(),
+                    "Apply Link": base_url+"/job/"+apply_link+"/apply".strip()}
+            
+            #Click next button
+            next_button.click() 
+                
         await page.close()
