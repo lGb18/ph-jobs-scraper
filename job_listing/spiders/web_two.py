c519279e5fb1417064c1a1310a7f4ea00c3b575c
Levi Barroga <levibarroga18@gmail.com>
2025-08-07 12:32:06 +0800

    PhilJobNet Scraper
    Add web address for spider web_two. Add sync JOB TITLE extraction.

--- job_listing/spiders/web_two.py
+++ job_listing/spiders/web_two.py
@@ -1,25 +1,37 @@
-# import scrapy
+import scrapy
+from scrapy_playwright.page import PageMethod
 
-# class WebTwo(scrapy.Spider):
-#     name = "web_two"
-#     start_urls = ["https://onlinejobs.ph"]
+class WebTwo(scrapy.Spider):
+    name = "web_two"
+    start_urls = ["https://philjobnet.gov.ph/job-vacancies/"]
 
-#     def start_requests(self):
-#         for url in self.start_urls:
-#             yield scrapy.Request(
-#                 url,
-#                 meta={
-#                     "playwright": True,
-#                     "playwright_include_page": True,
-#                     "playwright_page_methods": [   
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url,
+                meta={
+                    "playwright": True,
+                    "playwright_include_page": True,
+                    "playwright_page_methods": [   
+                        PageMethod("fill", "#ctl00_BodyContentPlaceHolder_searchterm", "developer"),
+                        PageMethod("click","#ctl00_BodyContentPlaceHolder_Button1")
+                        
+                    ]
+                },
+                callback=self.parse,
+            )
+
+    def parse(self, response):
+        page = response.meta["playwright_page"]
+        base_url = "https://philjobnet.gov.ph/job-vacancies"
+
+        for jobCard in response.css("div.jobcard"):
+            title = jobCard.css('h1.jobtitle::text').get()
+
+            yield {
+                "JO": title.strip()
+
+            }
             
-#                     ]
-#                 },
-#                 callback=self.parse,
-#             )
-
-#     async def parse(self, response):
-#         page = response.meta["playwright_page"]
-#         base_url = "https://onlinejobs.ph"
         
              
