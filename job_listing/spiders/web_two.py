af773edf0790c1e92227eb40e4f7fe44ab544c51
Levi Barroga <levibarroga18@gmail.com>
2025-08-09 20:53:20 +0800

    Parse method now uses async for page methods

--- job_listing/spiders/web_two.py
+++ job_listing/spiders/web_two.py
@@ -14,7 +14,7 @@ class WebTwo(scrapy.Spider):
                     "playwright_include_page": True,
                     "playwright_page_methods": [   
                         PageMethod("fill", "#ctl00_BodyContentPlaceHolder_searchterm", "developer"),
-                        PageMethod("click", 'input[type="submit"][value="Search"]'),
+                        # PageMethod("click", 'input[type="submit"][value="Search"]'),
                         
                         
                     ]
@@ -22,7 +22,7 @@ class WebTwo(scrapy.Spider):
                 callback=self.parse,
             )
 
-    def parse(self, response):
+    async def parse(self, response):
         page = response.meta["playwright_page"]
         base_url = "https://philjobnet.gov.ph/job-vacancies"
         # await page.locator('input [id="ctl00_BodyContentPlaceHolder_Button1"]').click()
@@ -34,6 +34,24 @@ class WebTwo(scrapy.Spider):
                 "JO": title.strip()
 
             }
-            
+        page_num = 1
         
-             
+        next_page = response.css('a::attr(href)')
+        if await page.is_visible('a[href="javascript:__doPostBack(\'ctl00$BodyContentPlaceHolder$GridView1\',\'Page$Last\')"]'):
+            page_num += 1
+            await page.evaluate(f"window.__doPostBack('ctl00$BodyContentPlaceHolder$GridView1','Page$ + {page_num}')")
+            current_url = response.url
+            yield scrapy.Request(
+               url=current_url,
+                meta={
+                    "playwright": True,
+                    "playwright_include_page": True,
+                    "playwright_page_methods": [
+                       
+                        PageMethod("wait_for_load_state", "domcontentloaded"),
+                        PageMethod("wait_for_load_state", "networkidle"),
+                    ]
+                },
+                callback=self.parse,
+            )
+        
