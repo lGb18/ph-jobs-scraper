6769acf92f285e1b926d23fddfde749ca4afb6d3
Levi Barroga <levibarroga18@gmail.com>
2025-08-10 21:23:41 +0800

    Remove commented codes from previous version

--- job_listing/spiders/web_two.py
+++ job_listing/spiders/web_two.py
@@ -6,33 +6,14 @@ class WebTwo(scrapy.Spider):
     name = "web_two"
     start_urls = ["https://philjobnet.gov.ph/job-vacancies/"]
 
-    # def start_requests(self):
-    #     for url in self.start_urls:
-    #         yield scrapy.Request(
-    #             url,
-    #             meta={
-    #                 "playwright": True,
-    #                 "playwright_include_page": True,
-    #                 "playwright_page_methods": [   
-    #                     PageMethod("fill", "#ctl00_BodyContentPlaceHolder_searchterm", "developer"),
-    #                     # PageMethod("click", 'input[type="submit"][value="Search"]'),
-                        
-                        
-    #                 ]
-    #             },
-    #             callback=self.parse,
-    #         )
-
     async def parse(self, response):
         async with async_playwright() as p:
-        # Launch browser in non-headless mode with DevTools
+        
             browser = await p.chromium.launch(headless=False, devtools=True)
             context = await browser.new_context()
             page = await context.new_page()
 
             await page.goto(response.url)
-        # base_url = "https://philjobnet.gov.ph/job-vacancies"
-        # await page.locator('input [id="ctl00_BodyContentPlaceHolder_Button1"]').click()
             page_num = 1
             for i in range(10):
                 next_page = page_num + 1
@@ -50,29 +31,5 @@ class WebTwo(scrapy.Spider):
                         "JO": title.strip()
 
                     }
-        # page_num = 1
-        
-        # data_table = page.locator('#ctl00_BodyContentPlaceHolder_GridView1')
-        
 
-        # while True:
-           
-        #     next_page = data_table.get_by_role('link', name=f'{page_num + 1}', exact=True)
-        #     await next_page.click()
-        #     page_num += 1 
-        #     page.wait_for_timeout(3000)
-        #     yield scrapy.Request(
-        #         url=page.url,
-        #         meta={
-        #             "playwright": True,
-        #             "playwright_include_page": True,
-        #             "playwright_page_methods": [
-        #                 PageMethod("wait_for_selector", '#ctl00_BodyContentPlaceHolder_GridView1'),
-        #                 PageMethod("wait_for_load_state", "networkidle"),
-        #                 PageMethod("wait_for_load_state", "domcontentloaded"),
-
-        #             ],
-        #         },
-        #         callback=self.parse,
-        #     )
         
