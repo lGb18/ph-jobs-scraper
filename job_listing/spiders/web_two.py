36da1095affaa93e69d545da20b2d2de1b292ce2
Levi Barroga <levibarroga18@gmail.com>
2025-08-10 18:31:03 +0800

    Scraping with AJAX Pagination
    Remove scrapy requests and use playwright directly.
    
    Website dynamically changes its content at the exact same page which the scrapy http request won't be able to catch. Only the initial page loaded regardless of page clicks would be loaded. With playwright instance the page content can change at the same instance hence the use of async playwright directly.

--- job_listing/spiders/web_two.py
+++ job_listing/spiders/web_two.py
@@ -1,61 +1,78 @@
 import scrapy
-from scrapy_playwright.page import PageMethod
+# from scrapy_playwright.page import PageMethod
+from playwright.async_api import async_playwright
 
 class WebTwo(scrapy.Spider):
     name = "web_two"
     start_urls = ["https://philjobnet.gov.ph/job-vacancies/"]
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(
-                url,
-                meta={
-                    "playwright": True,
-                    "playwright_include_page": True,
-                    "playwright_page_methods": [   
-                        PageMethod("fill", "#ctl00_BodyContentPlaceHolder_searchterm", "developer"),
-                        # PageMethod("click", 'input[type="submit"][value="Search"]'),
+    # def start_requests(self):
+    #     for url in self.start_urls:
+    #         yield scrapy.Request(
+    #             url,
+    #             meta={
+    #                 "playwright": True,
+    #                 "playwright_include_page": True,
+    #                 "playwright_page_methods": [   
+    #                     PageMethod("fill", "#ctl00_BodyContentPlaceHolder_searchterm", "developer"),
+    #                     # PageMethod("click", 'input[type="submit"][value="Search"]'),
                         
                         
-                    ]
-                },
-                callback=self.parse,
-            )
+    #                 ]
+    #             },
+    #             callback=self.parse,
+    #         )
 
     async def parse(self, response):
-        page = response.meta["playwright_page"]
-        base_url = "https://philjobnet.gov.ph/job-vacancies"
+        async with async_playwright() as p:
+        # Launch browser in non-headless mode with DevTools
+            browser = await p.chromium.launch(headless=False, devtools=True)
+            context = await browser.new_context()
+            page = await context.new_page()
+
+            await page.goto(response.url)
+        # base_url = "https://philjobnet.gov.ph/job-vacancies"
         # await page.locator('input [id="ctl00_BodyContentPlaceHolder_Button1"]').click()
+            page_num = 1
+            for i in range(10):
+                next_page = page_num + 1
+                nexts_page = page.locator('#ctl00_BodyContentPlaceHolder_GridView1')
+                # next_page.click()
+                next_pagge = nexts_page.get_by_role('link', name=f'{str(next_page)}', exact=True)
+                await next_pagge.click()
 
-        for jobCard in response.css("div.jobcard"):
-            title = jobCard.css('h1.jobtitle::text').get()
+                page_num +=1
+                j_title = await page.locator('h1').all()
+                for jobCard in j_title:
+                    title = await jobCard.text_content()
 
-            yield {
-                "JO": title.strip()
+                    yield {
+                        "JO": title.strip()
 
-            }
-        page_num = 1
+                    }
+        # page_num = 1
         
-        data_table = page.locator('#ctl00_BodyContentPlaceHolder_GridView1')
+        # data_table = page.locator('#ctl00_BodyContentPlaceHolder_GridView1')
         
 
-        while True:
+        # while True:
            
-            next_page = data_table.get_by_role('link', name=f'{page_num + 1}', exact=True)
-            await next_page.click()
-            page_num += 1 
-            yield scrapy.Request(
-                url=page.url,
-                meta={
-                    "playwright": True,
-                    "playwright_include_page": True,
-                    "playwright_page_methods": [
-                        PageMethod("wait_for_selector", '#ctl00_BodyContentPlaceHolder_GridView1'),
-                        PageMethod("wait_for_load_state", "networkidle"),
-                        PageMethod("wait_for_load_state", "domcontentloaded"),
+        #     next_page = data_table.get_by_role('link', name=f'{page_num + 1}', exact=True)
+        #     await next_page.click()
+        #     page_num += 1 
+        #     page.wait_for_timeout(3000)
+        #     yield scrapy.Request(
+        #         url=page.url,
+        #         meta={
+        #             "playwright": True,
+        #             "playwright_include_page": True,
+        #             "playwright_page_methods": [
+        #                 PageMethod("wait_for_selector", '#ctl00_BodyContentPlaceHolder_GridView1'),
+        #                 PageMethod("wait_for_load_state", "networkidle"),
+        #                 PageMethod("wait_for_load_state", "domcontentloaded"),
 
-                    ],
-                },
-                callback=self.parse,
-            )
+        #             ],
+        #         },
+        #         callback=self.parse,
+        #     )
         
