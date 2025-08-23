a595ab4e0592813868c6207e901904a05385a9c7
Levi Barroga <levibarroga18@gmail.com>
2025-08-23 19:12:56 +0800

    Change URL to API pagination URL directly with minimum of 500 list for each request

--- job_listing/spiders/web_four.py
+++ job_listing/spiders/web_four.py
@@ -3,7 +3,7 @@ from scrapy_playwright.page import PageMethod
 import json
 class WebFour(scrapy.Spider):
     name = "web_four"
-    start_urls = ["https://www.kalibrr.com/_next/data/-fHXcxPVjzAEIkO7CkhrK/en/home/te/developer.json?param=te&param=developer"]
+    start_urls = ["https://www.kalibrr.com/kjs/job_board/search?limit=500&offset=0&text=developer"]
     custom_settings = {
         'CLOSESPIDER_PAGECOUNT': 5,
         }
@@ -12,5 +12,5 @@ class WebFour(scrapy.Spider):
     def parse(self, response):
         web_data = json.loads(response.body)
 
-        job_list = [{ "Job Title": jobs["name"], "Company": jobs["companyName"], "Job Link": f'https://www.kalibrr.com/c/{jobs["company"]["code"]}/jobs/{jobs["id"]}/{jobs["slug"]}' } for jobs in web_data['pageProps']['jobs']]
+        job_list = [{ "Job Title": jobs["name"], "Company": jobs["company_name"], "Job Link": f'https://www.kalibrr.com/c/{jobs["company"]["code"]}/jobs/{jobs["id"]}/{jobs["slug"]}' } for jobs in web_data['jobs']]
         yield from job_list
