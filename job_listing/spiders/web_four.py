9539d5e492412759cf2943098a266f95beb1a0c1
Levi Barroga <levibarroga18@gmail.com>
2025-08-23 18:32:48 +0800

    Change scrapy method to parse only
    CSS selectors is not working for this specific site, as an alternative, an API call can be made to still scrape the data which is in this case through output through a JSON file. Previous function is excluded entirely as they are currently redundant.
    
    A list comprehension loop is used to get the value of primary data of each job listings. JSON file contains job data with max of 15 lists.

--- job_listing/spiders/web_four.py
+++ job_listing/spiders/web_four.py
@@ -1,6 +1,6 @@
 import scrapy
 from scrapy_playwright.page import PageMethod
-
+import json
 class WebFour(scrapy.Spider):
     name = "web_four"
     start_urls = ["https://www.kalibrr.com/_next/data/-fHXcxPVjzAEIkO7CkhrK/en/home/te/developer.json?param=te&param=developer"]
@@ -8,31 +8,9 @@ class WebFour(scrapy.Spider):
         'CLOSESPIDER_PAGECOUNT': 5,
         }
     
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(
-                url,
-                meta={
-                    "playwright": True,
-                    "playwright_include_page": True,
-                    "playwright_page_methods": [
-                        PageMethod("wait_for_load_state", "domcontentloaded"),
-                        PageMethod("wait_for_load_state", "networkidle"),                           
-                    ],
-            
-                },
-                callback=self.parse,
-        )
-
+    
     def parse(self, response):
-        page = response.meta["playwright_page"]
+        web_data = json.loads(response.body)
 
-        print(response.body)
-        # for jobcards in response.css('main'):
-        #     title = jobcards.css('h2.a::text').get()
-        #     job_link = jobcards.css('h2.a::attr(href)').get()
-
-        #     yield {
-        #         "Job Title": title.strip() if title else None,
-        #         "Job Link": job_link.strip() if title else None
-        #     }
+        job_list = [{ "Job Title": jobs["name"], "Company": jobs["companyName"] } for jobs in web_data['pageProps']['jobs']]
+        yield from job_list
