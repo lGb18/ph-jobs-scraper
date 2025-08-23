0ec029b811317f72e0c7e58936c7a0920f136f78
Levi Barroga <levibarroga18@gmail.com>
2025-08-23 18:56:24 +0800

    Add job link as new scrape data reference

--- job_listing/spiders/web_four.py
+++ job_listing/spiders/web_four.py
@@ -12,5 +12,5 @@ class WebFour(scrapy.Spider):
     def parse(self, response):
         web_data = json.loads(response.body)
 
-        job_list = [{ "Job Title": jobs["name"], "Company": jobs["companyName"] } for jobs in web_data['pageProps']['jobs']]
+        job_list = [{ "Job Title": jobs["name"], "Company": jobs["companyName"], "Job Link": f'https://www.kalibrr.com/c/{jobs["company"]["code"]}/jobs/{jobs["id"]}/{jobs["slug"]}' } for jobs in web_data['pageProps']['jobs']]
         yield from job_list
