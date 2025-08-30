644b524f2f9134b17c45741d3e3e47f6631f87e5
Levi Barroga <levibarroga18@gmail.com>
2025-08-30 13:31:42 +0800

    Add scrape loop

--- job_listing/spiders/web_five.py
+++ job_listing/spiders/web_five.py
@@ -18,6 +18,20 @@ class WebFive(scrapy.Spider):
             await page.locator('#rc_select_1').click()
             await page.locator('#rc_select_1').fill('developer')
             bttn = page.get_by_role('search')
-            await bttn.get_by_text('Search').click()
+            await bttn.get_by_text('Search', exact=True).click()
+            
 
+            for i in range(10):
+                j_title = await page.locator('h3 [class="index_pc_jobHireTopTitle__zVsw_"]').all()
+                # j_salary = await page.locator('span[class="index_pc_salaryText__k3HPE"]').all()
+                for i in zip(j_title):
+                    title = await i.text_content()
+                    # salary = await j.text_content()
 
+                    yield {
+                        "Job Title": title.strip() if title else None,
+                       
+                    }
+            
+            
+            await browser.close()
