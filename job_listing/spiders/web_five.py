47e5aba72ed8693cda75382177ae407fe3852d19
Levi Barroga <levibarroga18@gmail.com>
2025-09-02 20:56:32 +0800

    Fix selectors and remove pagination loop

--- job_listing/spiders/web_five.py
+++ job_listing/spiders/web_five.py
@@ -17,21 +17,24 @@ class WebFive(scrapy.Spider):
 
             await page.locator('#rc_select_1').click()
             await page.locator('#rc_select_1').fill('developer')
-            bttn = page.get_by_role('search')
-            await bttn.get_by_text('Search', exact=True).click()
+            await page.locator('#rc_select_1').press('Enter')
+            await page.wait_for_selector('div [class="style_contentLeft__RHYay"]', state='visible', timeout=None)
+            await page.wait_for_load_state(state='load', timeout=None)
             
+            
+            j_title = await page.locator('div [data-sentry-component="JobCardPc"] h3').all()
+            j_salary = await page.locator('div [data-sentry-component="JobCardPc"] span.index_pc_salaryText__k3HPE').all()
+            # j_salary = await page.locator('span[class="index_pc_salaryText__k3HPE"]').all()
+            for i, j in zip(j_title, j_salary):
+                title = await i.text_content()
+                salary = await j.text_content()
+                yield {
+                        "Job Title": title.strip(),
+                        "Salary": salary.strip()
+                }
+               
 
-            for i in range(10):
-                j_title = await page.locator('h3 [class="index_pc_jobHireTopTitle__zVsw_"]').all()
-                # j_salary = await page.locator('span[class="index_pc_salaryText__k3HPE"]').all()
-                for i in zip(j_title):
-                    title = await i.text_content()
-                    # salary = await j.text_content()
-
-                    yield {
-                        "Job Title": title.strip() if title else None,
-                       
-                    }
+                    
             
-            
-            await browser.close()
+            await page.pause()
+           
