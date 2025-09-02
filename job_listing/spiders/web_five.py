2a5ecfb861ca7a02346407921460a0589784df4d
Levi Barroga <levibarroga18@gmail.com>
2025-09-03 06:50:49 +0800

    Add jobsite as temporary link and cleanup

--- job_listing/spiders/web_five.py
+++ job_listing/spiders/web_five.py
@@ -15,19 +15,21 @@ class WebFive(scrapy.Spider):
 
             await page.goto(response.url)
 
+            #Search Action
             await page.locator('#rc_select_1').click()
             await page.locator('#rc_select_1').fill('developer')
             await page.locator('#rc_select_1').press('Enter')
+
+            #Wait for Load
             await page.wait_for_selector('div [class="style_contentLeft__RHYay"]', state='visible', timeout=None)
             await page.wait_for_load_state(state='load', timeout=None)
             
-            
+            #CSS References
             j_title = await page.locator('div [data-sentry-component="JobCardPc"] h3').all()
             j_salary = await page.locator('div [data-sentry-component="JobCardPc"] span.index_pc_salaryText__k3HPE').all()
             j_company = await page.locator('div [data-sentry-component="JobCardPc"] p').all()
             j_location = await page.locator('div [data-sentry-component="JobCardPc"] span.index_pc_jobCardLocationItem__Gzujh:nth-child(3)').all()
             
-            # j_salary = await page.locator('span[class="index_pc_salaryText__k3HPE"]').all()
             for i, j, k, l in zip(j_title, j_salary, j_company, j_location):
                 title = await i.text_content()
                 salary = await j.text_content()
@@ -37,11 +39,9 @@ class WebFive(scrapy.Spider):
                         "Job Title": title.strip(),
                         "Salary": salary.strip(),
                         "Company": company.strip(),
-                        "Location": location.strip() 
+                        "Location": location.strip(),
+                        "Link": response.url
                 }
-               
-
-                    
-            
-            await page.pause()
+                
+            await page.close()
            
